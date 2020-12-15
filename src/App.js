b2fae9e4092462542ff73b788aaa98b3d35295a8
Johnatan Brayan <johnatanbrayan1@gmail.com>
2020-12-15 09:17:24 -0300

    Make a box 2D

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.scss';
 
+import { Canvas } from 'react-three-fiber';
+
 function App() {
-  return (
-    <div className="App">
-    </div>
+  return ( 
+    <>
+      <Canvas>
+        <mesh>
+          <boxBufferGeometry attach='geometry' args={ [ 1, 1, 1 ] }/>
+          <meshStandardMaterial attach='material' />
+        </mesh>
+      </Canvas> 
+    </> 
   );
 }
 
