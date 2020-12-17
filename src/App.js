726a42e520e7be1c249402bf2220d13daab3c95a
Johnatan Brayan <johnatanbrayan1@gmail.com>
2020-12-17 14:00:29 -0300

    Make a cube with efect spinning

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.scss';
+import React , { useRef } from 'react';
+import { Canvas, useFrame } from 'react-three-fiber';
 
-import { Canvas } from 'react-three-fiber';
+const Box = ( ) => {
+  const mesh = useRef( null );
+  useFrame( () => ( mesh.current.rotation.x = mesh.current.rotation.y += 0.01 ) );
+
+  return (
+    <mesh ref={ mesh }>
+      <boxBufferGeometry attach='geometry' args={ [ 1, 1, 1 ] }/>
+      <meshStandardMaterial attach='material' />
+    </mesh>
+  )
+}
 
 function App() {
+  
   return ( 
     <>
       <Canvas>
-        <mesh>
-          <boxBufferGeometry attach='geometry' args={ [ 1, 1, 1 ] }/>
-          <meshStandardMaterial attach='material' />
-        </mesh>
+        <Box />
       </Canvas> 
     </> 
   );
