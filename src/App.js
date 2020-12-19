ee71a46ef0a0f28ab6ec923828de83f64cb191d5
Johnatan Brayan <johnatanbrayan1@gmail.com>
2020-12-19 16:33:13 -0300

    Set action click on cube

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.scss';
-import React , { useRef } from 'react';
+import React , { useRef, useState } from 'react';
 import { Canvas, useFrame } from 'react-three-fiber';
 import { MeshWobbleMaterial, OrbitControls, softShadows } from 'drei';
+import { useSpring, a } from 'react-spring/three';
 
 softShadows();
 
@@ -9,11 +10,16 @@ const SpinninMesh = ( { position, args, color, speed } ) => {
   const mesh = useRef( null );
   useFrame( () => ( mesh.current.rotation.x = mesh.current.rotation.y += 0.01 ) );
 
+  const [ expand, setExpand ] = useState( false );
+  const props = useSpring({
+    scale: expand ? [ 1.4, 1.4, 1.4 ] : [ 1, 1, 1 ],
+  });
+
   return (
-    <mesh castShadow position={ position } ref={ mesh }>
+    <a.mesh onClick={ () => setExpand(!expand) } scale={ props.scale } castShadow position={ position } ref={ mesh }>
       <boxBufferGeometry attach='geometry' args={ args }/>
       <MeshWobbleMaterial attach='material' color= { color } speed={ speed } factor={ 0.6 } />
-    </mesh>
+    </a.mesh>
   )
 }
 
@@ -24,10 +30,7 @@ function App() {
       <Canvas shadowMap colorManagement camera={ { position: [ -5, 2, 10 ], fov: 60 } }>
         <ambientLight intensity={ .3 } />
 
-        <directionalLight 
-          castShadow
-          position={[ 0, 10, 0 ]}
-          intensity={ 1.5 }
+        <directionalLight castShadow position={[ 0, 10, 0 ]} intensity={ 1.5 }
           shadow-mapSize-width={ 1024 }
           shadow-mapSize-height={ 1024 }
           shadow-camera-far={ 50 }
@@ -44,14 +47,13 @@ function App() {
           <mesh receiveShadow rotation={[ -Math.PI / 2, 0, 0 ]} position={[ 0, -3, 0 ]}>
             <planeBufferGeometry attach='geometry' args={[ 100, 100 ]} />
             <shadowMaterial attach='material' opacity={ .1 }/>
-            {/* <meshStandardMaterial attach='material' color={ "yellow" } /> */}
           </mesh>
-        </group>
 
-        <SpinninMesh position={[ 0, 1, 0 ]} args={[ 3, 2, 1 ]} color='lightblue' speed={ 2 }/>
-        <SpinninMesh position={[ -2, 1, -5 ]} color='pink' speed={ 6 }/>
-        <SpinninMesh position={[ 5, 1, -2 ]} color='lightgreen' speed={ 6 }/>
-        <OrbitControls />
+          <SpinninMesh position={[ 0, 1, 0 ]} args={[ 3, 2, 1 ]} color='lightblue' speed={ 2 }/>
+          <SpinninMesh position={[ -2, 1, -5 ]} color='pink' speed={ 6 }/>
+          <SpinninMesh position={[ 5, 1, -2 ]} color='lightgreen' speed={ 6 }/>
+          <OrbitControls />
+        </group>
       </Canvas> 
     </> 
   );
